ce4fd593591ef91a1ea7fd12c20d24e27ac9472f
sirawit suwannakin <lglriwork@hotmail.com>
2022-05-23 01:34:39 +0700

    [Edit] fix lll

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ from flask_cors import CORS
 app = APIFlask(__name__)
 CORS(app)
 
-uploads_dir = '/usr/src/app/static'
-app.config['UPLOAD_FOLDER'] = uploads_dir
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get('/')
 def say_hello():
+    print(UPLOAD_FOLDER)
     # returning a dict equals to use jsonify()
     return {'message': 'Hello!'}
 
@@ -26,14 +28,16 @@ class ImageSchema(Schema):
 @app.input(ImageSchema, location='files')
 def upload_image(data):
     f = data['image']
+
     print(f.filename)
+
     filename = secure_filename(f.filename)
-    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    f.save(os.path.join(UPLOAD_FOLDER, filename))
     class_names = ['cbb', 'cbsd', 'cgm', 'cmd', 'healthy']
     model_test = tf.keras.models.load_model('./my_model.h5')
 
     img = tf.keras.utils.load_img(
-        f'{app.config["UPLOAD_FOLDER"]}/{filename}', target_size=(256, 256)
+        f'{UPLOAD_FOLDER}/{filename}', target_size=(256, 256)
     )
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create a batch
@@ -41,8 +45,8 @@ def upload_image(data):
     predictions = model_test.predict(img_array)
     score = tf.nn.softmax(predictions[0])
 
-    if os.path.exists('/'.join([app.config['UPLOAD_FOLDER'], filename])):
-        os.remove('/'.join([app.config['UPLOAD_FOLDER'], filename]))
+    if os.path.exists('/'.join([UPLOAD_FOLDER, filename])):
+        os.remove('/'.join([UPLOAD_FOLDER, filename]))
 
     return {'message': "This image most likely belongs to {} with a {:.2f} percent confidence."
         .format(class_names[np.argmax(score)], 100 * np.max(score))}, 200
